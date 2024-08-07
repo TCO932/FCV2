0b9430aaaafee67ccabc7afce3a52bbf6fdf0669
TCO932 <chirkof.02@gmail.com>
2024-08-08 02:32:28 +0400

    usage recalcSpeedSubtree and rootItemSpeed

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import sys
 import time
 
 from PyQt6 import QtWidgets
-from PyQt6.QtCore import QModelIndex
 from PyQt6.QtWidgets import QApplication
 
 import FCV2 as fcv2
 from classes import EffectedMachine, ItemMeta, ItemTree
 from data import *
-from widgets.MainWindow import MainWindow
 from widgets.Graph import GraphView, Node
 from widgets.ItemsListTable import ItemsListTableView
 from widgets.ItemTable import ItemTableView
 from widgets.MachineWidget import MachineWidget
+from widgets.MainWindow import MainWindow
 
 
 def time_it(func):
@@ -26,56 +25,76 @@ def time_it(func):
     return wrapper
 
 global speedTree
+speedTree = None
+global item
 
-def itemClickHandler(item: Item):
+def itemClickHandler(selectedItem: Item):
     global speedTree
+    global item
+    item = selectedItem
     itemMeta = ItemMeta.fromItem(
         item, 
-        speed=0.1, 
+        speed=1, 
         effectedMachine=EffectedMachine.fromMachine(
-            ASSENBLING_MACHINE_3,
-            modules=[PRODUCTIVITY_MODULE_3, PRODUCTIVITY_MODULE_3, PRODUCTIVITY_MODULE_3, PRODUCTIVITY_MODULE_3],
-            beaconsNumber=12
+            ASSENBLING_MACHINE_1,
+            modules=[],
+            beaconsNumber=0
+        )
+    )
+    tree = ItemTree()
+    tree.addRoot(itemMeta)
+    graphicsView.build_graph(tree)
+    graphicsView.selectNode(itemMeta)
+
+    mainWindow.tabWidget.setCurrentIndex(1)
+
+def rootItemSpeedSettedHandler(speed: float):
+    global speedTree
+    global item
+    itemMeta = ItemMeta.fromItem(
+        item, 
+        speed=speed, 
+        effectedMachine=EffectedMachine.fromMachine(
+            ASSENBLING_MACHINE_1,
+            modules=[],
+            beaconsNumber=0
         )
     )
 
     speedTree = fcv2.buildSpeedTree(itemMeta)
     graphicsView.build_graph(speedTree)
-    graphicsView.selectNode(itemMeta)
-
-    mainWindow.tabWidget.setCurrentIndex(1)
+    print(speed)
 
 def buildTree(itemMeta: ItemMeta, effectedMachine: EffectedMachine):
     updTree = fcv2.buildChildrenSpeed(itemMeta, effectedMachine, speedTree)
     graphicsView.build_graph(updTree)
 
 def nodeSelectioHandler(node: Node):
-    print('node selected')
     itemTableView.setItem(node.itemMeta)
-    machineWidget.setModel(node.itemMeta.effectedMachine)
+    machineWidget.setModel(node.itemMeta)
 
 def machineSetHandler(effectedMachine: EffectedMachine):
     global speedTree
     currentItemMeta = itemTableView.itemMeta
     currentItemMeta.effectedMachine = effectedMachine
-    speedSubTree = fcv2.buildSpeedTree(currentItemMeta)
-    speedTree.replaceNodeWithSubTree(currentItemMeta, speedSubTree)
-    graphicsView.build_graph(speedTree)
-    print(currentItemMeta)
+    if speedTree:
+        fcv2.recalcSpeedSubtree(currentItemMeta, speedTree)
+    else:
+        speedTree = fcv2.buildSpeedTree(currentItemMeta)
 
-def rootItemSpeedSettedHandler(speed: float):
-    print(speed)
+    graphicsView.build_graph(speedTree)
+    graphicsView.selectNode(currentItemMeta)
+    print(currentItemMeta)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
     machineWidget = MachineWidget()
     itemTableView = ItemTableView()
     itemsListTableView = ItemsListTableView(ITEMS)
 
     graphicsView = GraphView(itemTableView)
-    mainWindow = MainWindow(window, itemsListTableView, graphicsView, itemTableView, machineWidget)
+    mainWindow = MainWindow(itemsListTableView, graphicsView, itemTableView, machineWidget)
 
     itemsListTableView.itemClicked.connect(itemClickHandler)
     
